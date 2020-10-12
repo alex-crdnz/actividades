a85455b4f312b4805103729fa8a6135f6713795e
cardenas <70241684+alex-crdnz@users.noreply.github.com>
2020-10-12 16:00:42 -0500

    actualizacion

---
+++ Actividad_8_Cartas/src/poker/Cards.java
@@ -0,0 +1,13 @@
+package poker;
+
+public class Cards {
+    //atributos de las cartas
+    String Palo,Color,Valor;
+    //constructor para establecer valor por determinado
+    public Cards(String Palo,String Color,String Valor){
+        this.Color=Color;
+        this.Valor=Valor;
+        this.Palo = Palo;
+    }
+
+}
